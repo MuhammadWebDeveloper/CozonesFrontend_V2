ed911db43d5c5f48e2f8c0461745227408042cef
Captain Developer <muhammadcaptain303@gmail.com>
2026-06-08 08:58:22 +0500

    soved many small bugs

--- src/chat-frontend/utils/chatconstants.js
+++ src/chat-frontend/utils/chatconstants.js
@@ -1,2 +1,2 @@
-// export const ChatUrl = "https://cozoens-backend-v2.vercel.app";
-export const ChatUrl = "http://localhost:4343"; // For local development
+export const ChatUrl = "https://cozoens-backend-v2.vercel.app";
+// export const ChatUrl = "http://localhost:4343"; // For local development
