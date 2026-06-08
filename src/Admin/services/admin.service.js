90b7973e01f2f2b097f898ec67ce62e3ced240ff
Captain Developer <muhammadcaptain303@gmail.com>
2026-06-08 08:55:07 +0500

    soved many small bugs

--- src/Admin/services/admin.service.js
+++ src/Admin/services/admin.service.js
@@ -1,5 +1,5 @@
-const BASE_URL = " https://cozoens-backend-v2.vercel.app/api";
-// const BASE_URL = "http://localhost:4343/api";
+// const BASE_URL = " https://cozoens-backend-v2.vercel.app/api";
+const BASE_URL = "http://localhost:4343/api";
 
 // import BaseUrl from "../../sitecomponents//";
 
